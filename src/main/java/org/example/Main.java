2b2be65191fe74d6b3fab8da2804fd5767cdf05e
Slava Rybkin <yolka.sd@gmail.com>
2023-06-29 02:18:20 +0700

    added CRUD methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,72 @@ import org.example.database.db.DBStatement;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException, IllegalAccessException {
         DBConnection connection = new DBConnection();
+        DBStatement dbStatement = new DBStatement(connection.connect());
         System.out.println("Подключение к базе данных: " + connection.connect() + "\n" + "_____________________");
         while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите одну из команд:");
+            System.out.println("1. Создать отношение Users");
+            System.out.println("2. Удалить отношение Users");
+            System.out.println("3. Внести запись отношение Users");
+            System.out.println("4. Удалить запись в отношении Users по id");
+            System.out.println("5. Обновить запись в отношении Users по id");
 
+            int id;
+            int age;
+            String name;
+            switch (scanner.nextLine()) {
+                case "1":
+                    System.out.println("Статус операции: " + dbStatement.createTableUsers());
+                    break;
+                case "2":
+                    System.out.println("Статус операции: " + dbStatement.deleteTableUsers());
+                    break;
+                case "3":
+                    System.out.println("Введите данные (имя, возраст) для их внесения в отношение users");
+                    System.out.print("Имя: ");
+                    name = scanner.next();
+                    System.out.print("Возраст: ");
+                    age = scanner.nextInt();
+                    System.out.println("Статус операции: " + dbStatement.insertIntoTableUsers(name, age));
+                    break;
+                case "4":
+                    System.out.println("Введите id пользователя для удаления");
+                    System.out.print("id: ");
+                    id = scanner.nextInt();
+                    if (dbStatement.hasIdInTableUsers(id)) {
+                        dbStatement.deleteUserFromTableUsers(id);
+                        System.out.println("Статус операции: " + true);
+                    } else {
+                        System.out.println("Статус операции: " + false);
+                    }
+                    break;
+
+                case "5":
+                    System.out.println("Введите id пользователя для изменения данных");
+                    System.out.print("id: ");
+                    id = scanner.nextInt();
+                    if (dbStatement.hasIdInTableUsers(id)) {
+                        System.out.println("Введите данные (имя, возраст) для внесения новых данных в отношение users");
+                        System.out.print("Имя: ");
+                        name = scanner.next();
+                        System.out.print("Возраст: ");
+                        age = scanner.nextInt();
+                        dbStatement.updateUserFromTableUsers(id, name, age);
+                        System.out.println("Статус операции: " + true);
+
+                    } else {
+                        System.out.println("Статус операции: " + false);
+                    }
+                    break;
+            }
+
+            System.out.println("_____________________");
         }
     }
 }
--- src/main/java/org/example/database/db/DBStatement.java
+++ src/main/java/org/example/database/db/DBStatement.java
@@ -1,22 +1,115 @@
 package org.example.database.db;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBStatement {
     private Connection connection;
 
-    public DBStatement(Connection connection) throws SQLException {
+    public DBStatement(Connection connection) {
         this.connection = connection;
     }
 
-    public int executeInsert(String name, int age) throws SQLException {
-        String insertQuery = "INSERT INTO users (name, age) VALUES(?, ?);";
-        try (PreparedStatement statement = connection.prepareStatement(insertQuery)){
+    public boolean createTableUsers() {
+        String createTable = "CREATE TABLE users (" +
+                "id BIGSERIAL NOT NULL PRIMARY KEY, " +
+                "name VARCHAR(60) NOT NULL, " +
+                "age int NOT NULL" +
+                ");";
+        boolean result = false;
+        try {
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try (PreparedStatement statement = connection.prepareStatement(createTable)) {
+            statement.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            System.err.println("ОШИБКА: отношение \"users\" уже существует");
+        }
+
+        return result;
+    }
+
+    public boolean deleteTableUsers() {
+        String deleteTable = "DROP TABLE users;";
+        boolean result = false;
+        try (PreparedStatement statement = connection.prepareStatement(deleteTable)) {
+            statement.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            System.err.println("ОШИБКА: таблица \"users\" не существует");
+        }
+        return result;
+    }
+
+    public boolean insertIntoTableUsers(String name, int age) {
+        String insertInto = "INSERT INTO users (name, age) VALUES(?, ?);";
+        boolean result;
+        try (PreparedStatement statement = connection.prepareStatement(insertInto)) {
             statement.setString(1, name);
             statement.setInt(2, age);
-            return statement.executeUpdate();
+            statement.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+        return result;
+    }
+
+    public void deleteUserFromTableUsers(int id) {
+        String deleteUser = "DELETE FROM users WHERE id = ?;";
+        try (PreparedStatement statement = connection.prepareStatement(deleteUser)) {
+            statement.setInt(1, id);
+            int result = statement.executeUpdate();
+            if (result == 0) {
+                printErr(id);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateUserFromTableUsers(int id, String name, int age) {
+        String updateUser = "UPDATE users SET age = ?, name = ? WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(updateUser)) {
+            statement.setInt(1, age);
+            statement.setString(2, name);
+            statement.setInt(3, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean hasIdInTableUsers(int id) {
+        String selectCount = "SELECT COUNT(*) FROM users WHERE id = ?;";
+        boolean result = false;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(selectCount)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                if (resultSet.getInt(1) > 0) {
+                    result = true;
+                } else {
+                    printErr(id);
+                }
+            }
+            resultSet.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    private void printErr(int id) {
+        System.err.println("Запись по id " + id + " не найдена");
     }
 
 }
